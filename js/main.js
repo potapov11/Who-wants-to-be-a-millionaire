2c9d6154919b27232c326ebd7973accff4b18e97
Serezha <volos11223344@yandex.ru>
2023-03-21 17:51:47 +0300

    new com

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const questionTitle = document.querySelector('.question-title'),
-      questionHTMLList = document.querySelector('.question-list');
+      questionHTMLList = document.querySelector('.question-list'),
+      btn50 = document.querySelector('.bnt50');
 
 let count = 0;
 
@@ -18,18 +19,33 @@ function createQuestion() {
 }
 
 function getAnswer() {
+
+  
   questionHTMLList.addEventListener('click', (e) => {
     console.log('есть клик');
     if(e.target.tagName !== 'LI') return;
-    if(e.target.textContent == questionsL[count].correctAnswer) {
+    if(e.target.textContent == questionsL[count].correctAnswer) { //Если ответ правильный
       console.log(questionsL[count].correctAnswer);
       alert('все верно');
       count++;
       console.log(count);
       createQuestion();
+    } else {
+      alert('попробуй еще раз');
     }
+
+  });
+}
+
+
+function deleteTwoAnswer() {
+  questionsL[count].incorrectAnswer.forEach(item => {
+    console.log(item);
+    questionHTMLList.innerHTML += `<li class="question-item">${item}</li>`;
   });
 }
 
 getAnswer();
+
+console.log(questionHTMLList.innerHTML);
 
