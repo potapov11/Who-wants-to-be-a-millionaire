7247276e87ea70b8daee47840a2b792d1d50e420
Serezha <volos11223344@yandex.ru>
2023-03-31 22:21:31 +0300

    добавляет подсказку зала

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const questionTitle = document.querySelector('.question-title'),
       btn50 = document.querySelector('.tips__50'),
       audio50 = document.getElementById('my-audio'),
       audioCall = document.getElementById('my-audiocall'),
+      audioPoll = document.getElementById('my-audiopoll'),
       btnCallFriend = document.querySelector('.callFriend'),
       callFriendModal = document.querySelector('.callfriendmodal'),
       callFriendInner= document.querySelector('.callfriendinner'),
@@ -12,7 +13,7 @@ const questionTitle = document.querySelector('.question-title'),
       tipPollAudience = document.querySelector('.pollAudience'),
       pollAudienceModal = document.querySelector('.pollaudiencemodal'),
       closemodalPoll = document.querySelector('.closemodalPoll'),
-      pollAnswers = document.querySelector('.pollAnswers');
+      pollAnswers = document.querySelector('.pollAnswers');      
 
 
       import {costWiningColumn} from './costWinning.js';
@@ -25,7 +26,7 @@ const questionTitle = document.querySelector('.question-title'),
 let questionRandItemPlus = questionRandItem;
 let questionRandItemHeavyPlus = questionRandItemHeavy;
 
-let count = 0;
+let count = 5;
 let deleteTwoAnswerCount = 0;
 
 createQuestion();
@@ -178,6 +179,16 @@ function stopAudioPlay() {
   console.log('stopAudio');
 }
 
+function playAdioPoll(){
+  audioPoll.play();
+  console.log('poll play');
+}
+function stopAudioPlayPoll() {
+  audioPoll.currentTime = 0;
+  audioPoll.pause();
+  console.log('stopAudio');
+}
+
 //Отложенные действия в подсказке звонок другу
 
 function getTimeoutAnswer(){
@@ -216,21 +227,100 @@ function getTimeoutHeavyAnswer(){
 //-------------------------------------------------//
 
 //Помощь зала
+// let pollAnswertext;
+let textHeight;
+function getRandomNumMore60(min, max) {
+  textHeight =  Math.floor(Math.random() * (max - min)) + min;
+  return textHeight;
+}
+getRandomNumMore60(60, 90);
+
+let textHeightMin;
+function getRandomNumLess60(min, max) {
+  textHeightMin =  Math.floor(Math.random() * (max - min)) + min;
+  return textHeight;
+}
+
 tipPollAudience.addEventListener('click', getPollTip, {once: true});
 
 function getPollTip() {
   pollAudienceModal.classList.toggle('show');
+  console.log(pollAudienceModal.classList.value);
+  if(pollAudienceModal.classList.value == 'pollaudiencemodal pollaudience hide show') {
+    playAdioPoll();
+  }
+
+  if(count < 5) {
+    showEasyPollAnswers();
+  } else {
+    showHeavyPollAnswers();
+  }
+  tipPollAudience.classList.add('tipOpacity');
+}
+
+
+closemodalPoll.addEventListener('click', function(){
+  pollAudienceModal.classList.toggle('show');
+  console.log(pollAudienceModal.classList.value);
+
+  if(pollAudienceModal.classList.value == 'pollaudiencemodal pollaudience hide') {
+    console.log('stop play');
+    stopAudioPlayPoll();
+  }
+});
+
+
+function showEasyPollAnswers() {
   questionsL[questionRandItemPlus].answers.forEach(item=>{
+
     pollAnswers.innerHTML+=`
     <div class = pollAnswercolumn>
     <div class = pollcolorcolumn></div>
     <p class='pollAnswertext'>${item}</p>
-    </div>`;
-    console.log('pollansw');
+    </div>`;    
+  });
+
+  const pollAnswertext = document.querySelectorAll('.pollAnswertext');
+  console.log(pollAnswertext);
+  pollAnswertext.forEach(itemText=>{
+    if(typeof(questionsL[questionRandItemPlus].correctAnswer) == 'number') {
+      questionsL[questionRandItemPlus].correctAnswer = String(questionsL[questionRandItemPlus].correctAnswer);
+      console.log(typeof(questionsL[questionRandItemPlus].correctAnswer));
+    }
+    if(itemText.textContent === questionsL[questionRandItemPlus].correctAnswer) {
+      itemText.previousElementSibling.style.cssText = `background-color: green; height: ${textHeight}%`;
+    } else{
+      getRandomNumLess60(10, 50);
+      itemText.previousElementSibling.style.cssText = `background-color: red; height: ${textHeightMin}%`;
+    }
   });
 }
 
-closemodalPoll.addEventListener('click', function(){
-  pollAudienceModal.classList.toggle('show');
-  console.log('pollclose');
-});
+function showHeavyPollAnswers() {
+  questionsHeavy[questionRandItemHeavyPlus].answers.forEach(item=>{
+
+    pollAnswers.innerHTML+=`
+    <div class = pollAnswercolumn>
+    <div class = pollcolorcolumn></div>
+    <p class='pollAnswertext'>${item}</p>
+    </div>`;    
+  });
+  // questionsHeavy[questionRandItemHeavyPlus].question}
+  const pollAnswertext = document.querySelectorAll('.pollAnswertext');
+  console.log(pollAnswertext);
+  pollAnswertext.forEach(itemText=>{
+    if(typeof(questionsHeavy[questionRandItemHeavyPlus].correctAnswer) == 'number') {
+      questionsHeavy[questionRandItemHeavyPlus].correctAnswer = String(questionsHeavy[questionRandItemHeavyPlus].correctAnswer);
+      console.log(typeof(questionsHeavy[questionRandItemHeavyPlus].correctAnswer));
+    }
+    // console.log(item.textContent);
+    if(itemText.textContent === questionsHeavy[questionRandItemHeavyPlus].correctAnswer) {
+      itemText.previousElementSibling.style.cssText = `background-color: green; height: ${textHeight}%`;
+    } else{
+      getRandomNumLess60(10, 50);
+      itemText.previousElementSibling.style.cssText = `background-color: red; height: ${textHeightMin}%`;
+    }
+  });
+  console.log(questionRandItemHeavyPlus);
+}
+
