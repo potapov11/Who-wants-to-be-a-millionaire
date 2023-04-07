2ad34d036758e43505f7ed26ca99771dfc0ac084
Serezha <volos11223344@yandex.ru>
2023-04-07 07:20:31 +0300

    добавляет неверные ответы

--- modules-js/heavyquestion.js
+++ modules-js/heavyquestion.js
@@ -4,63 +4,63 @@ export const questionsHeavy = [
     answers: ['2 августа', '19 марта', '2-е воскресение июля', 'Посл. воскресение июля'],
     correctAnswer: 'Посл. воскресение июля',
     correctPollAnswer: 'С',
-    incorrectAnswer: ['19 марта', '2 августа']
+    incorrectAnswer: ['B 19 марта', 'A 2 августа']
   },
   {
     question: 'Какой возможности лишен крупье казино?',
     answers: ['пригладить рукой волосы', 'снять пылинку с жилет', 'поправить галстук', 'достать платок из кармана'],
     correctAnswer: 'достать платок из кармана',
     correctPollAnswer: 'D',
-    incorrectAnswer: ['пригладить рукой волосы', 'снять пылинку с жилет']
+    incorrectAnswer: ['A пригладить рукой волосы', 'B снять пылинку с жилет']
   },
   {
     question: '	Кличка основателя группы Коррозия металла',
     answers: ['Скелет', 'Паук', 'Ручейник', 'Горшок'],
     correctAnswer: 'Паук',
     correctPollAnswer: 'B',
-    incorrectAnswer: ['Скелет', 'Горшок']
+    incorrectAnswer: ['A Скелет', 'B Горшок']
   },
   {
     question: 'Какой вопрос, по определению, не требует ответа?',
     answers: ['Каверзный', 'Философский', 'Экзаменационный', 'Риторический'],
     correctAnswer: 'Риторический',
     correctPollAnswer: 'D',
-    incorrectAnswer: ['Каверзный', 'Философский']
+    incorrectAnswer: ['A Каверзный', 'B Философский']
   },
   {
     question: 'В каких спортивных состязаниях не используется сетка?',
     answers: ['баскетбол', 'регби', 'бадминтон', 'метание молота'],
     correctAnswer: 'регби',
     correctPollAnswer: 'B',
-    incorrectAnswer: ['баскетбол', 'метание молота']
+    incorrectAnswer: ['A баскетбол', 'D метание молота']
   },
   {
     question: 'Кто из перечисленных ниже лиц НЕ является египетским фараоном?',
     answers: ['Сети', 'Акхенотеп', 'Смендес', 'Имхотеп'],
     correctAnswer: 'Имхотеп',
     correctPollAnswer: 'D',
-    incorrectAnswer: ['Смендес', 'Акхенотеп']
+    incorrectAnswer: ['C Смендес', 'B Акхенотеп']
   },
   {
     question: 'Что такое десница?',
     answers: ['Бровь', 'Шея', 'Глаз', 'Рука'],
     correctAnswer: 'Рука',
     correctPollAnswer: 'D',
-    incorrectAnswer: ['Бровь', 'Глаз']
+    incorrectAnswer: ['A Бровь', 'C Глаз']
   },
   {
     question: 'В какое море впадает река Урал?',
     answers: ['Чёрное', 'Азовское', 'Средиземное', 'Каспийское'],
     correctAnswer: 'Каспийское',
     correctPollAnswer: 'D',
-    incorrectAnswer: ['Средиземное', 'Азовское']
+    incorrectAnswer: ['C Средиземное', 'B Азовское']
   },
   {
     question: '	Кличка основателя группы Коррозия металла',
     answers: ['Скелет', 'Паук', 'Ручейник', 'Горшок'],
     correctAnswer: 'Паук',
     correctPollAnswer: 'B',
-    incorrectAnswer: ['Скелет', 'Горшок']
+    incorrectAnswer: ['A Скелет', 'D Горшок']
   },
   {
     question: 'Какое животное имеет второе название — кугуар?',
@@ -74,63 +74,63 @@ export const questionsHeavy = [
     answers: [4, 6, 3, 1],
     correctAnswer: 4,
     correctPollAnswer: 'D',
-    incorrectAnswer: [3, 1]
+    incorrectAnswer: ["C 3", "D 1"]
   },
   {
     question: 'Чем на Руси занимался золотарь?',
     answers: ['Добывал золото', 'Ковал оружие', 'Заведовал казной', 'Чистил отхожие места'],
     correctAnswer: 'Чистил отхожие места',
     correctPollAnswer: 'D',
-    incorrectAnswer: ['Добывал золото', 'Ковал оружие']
+    incorrectAnswer: ['A Добывал золото', 'B Ковал оружие']
   },
   {
     question: 'Как в старинной легенде звали дочь богатыря Байкала??',
     answers: ['Ангара', 'Лена', 'Бирюса', 'Тунгуска'],
     correctAnswer: 'Ангара',
     correctPollAnswer: 'A',
-    incorrectAnswer: ['Бирюса', 'Тунгуска']
+    incorrectAnswer: ['C Бирюса', 'D Тунгуска']
   },
   {
     question: 'Необычную серию чего во Франции выпустили с запахом шоколада?',
     answers: ['Монет', 'Купюр', 'Почтовых марок', 'Банковских карт'],
     correctAnswer: 'Почтовых марок',
     correctPollAnswer: 'C',
-    incorrectAnswer: ['купюр', 'монет']
+    incorrectAnswer: ['B Купюр', 'A монет']
   },
   {
     question: 'Какой герой русской классики сделал фальшивый портсигар из найденной на улице дощечки??',
     answers: ['Чичиков', 'Раскольников', 'Дубровский', 'Человек в футляре'],
     correctAnswer: 'Раскольников',
     correctPollAnswer: 'B',
-    incorrectAnswer: ['Человек в футляре', 'Чичиков']
+    incorrectAnswer: ['D Человек в футляре', 'A Чичиков']
   },
   {
     question: 'Как называется сказка Андерсена?',
     answers: ['Кукушка', 'Аист', 'Скворец', 'Соловей'],
     correctAnswer: 'Соловей',
     correctPollAnswer: 'D',
-    incorrectAnswer: ['Кукушка', 'Аист']
+    incorrectAnswer: ['A Кукушка', 'B Аист']
   },
   {
     question: 'Какую монету называли «пятиалтынный»?',
     answers: ['5 копеек', '10 копеек', '15 копеек', '20 копеек'],
     correctAnswer: '15 копеек',
     correctPollAnswer: 'C',
-    incorrectAnswer: ['20 копеек', '5 копеек']
+    incorrectAnswer: ['D 20 копеек', 'A 5 копеек']
   },
   {
     question: 'В каком кинофильме нет сцены на экзамене?',
     answers: ['Доживем до понедельника', 'Операция «Ы»', 'Старик Хоттабыч', 'Большая перемена'],
     correctAnswer: 'Доживем до понедельника',
     correctPollAnswer: 'A',
-    incorrectAnswer: ['Большая перемена', 'Старик Хоттабыч']
+    incorrectAnswer: ['D Большая перемена', 'A Старик Хоттабыч']
   },
   {
     question: 'Как греки сами называют свою страну?',
     answers: ['Паллала', 'Эллада', 'Колхида', 'Илиада'],
     correctAnswer: 'Эллада',
     correctPollAnswer: 'B',
-    incorrectAnswer: ['Паллала', 'Илиада']
+    incorrectAnswer: ['A Паллала', 'D Илиада']
   },
 ];
 console.log(questionsHeavy.length);
